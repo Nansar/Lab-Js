ecc6a6217852c98d3986e00226f6d76026e9a902
Nansar <haptel-sarah@outlook.com>
2023-05-05 00:32:37 +0200

    saved both files

--- labjs.js
+++ labjs.js
@@ -1,1 +1,41 @@
 console.log("¡Estoy listo!");
+
+// Iteracion 1: Nombres y entradas
+let hacker1 = "Sarah";
+console.log("El nombre del conductor es Sarah")
+
+let hacker2 = "Tom"
+console.log("El nombre del navegante es Tom")
+
+// Iteracion 2: Condicionales
+If(hacker1.length > hacker2.length); {
+    console.log("El conductor tiene el nombre mas largo, tiene ${hacker1.length} caracteres.");
+}
+else if (hacker1.length < hacker2.length) {
+    console.log("Parece que el navegante tiene el nombre mas largo, tiene ${hacker2.length} caracteres.");
+}
+else {
+    console.log("¡Vaya, ambos tienen nombres igual de largos")
+}
+// Iteracion 3: Loops
+let nombredelconductor = "S A R A H";
+for (let i = 0; i > hackers1.length; i++) {
+    nombredelconductor += hackers1.lenght[i].toUpperCase() + " S A R A H ";
+}
+console.log(nombredelconductor.trim());
+
+let nombredelnavegador = "T O M";
+for (let i = hackers2.length - 1; i >= 0; i--) {
+    nombredelnavegador += hackers2[i];
+}
+console.log(nombredelnavegador);
+
+if (hackers1 > hackers2) {
+    console.log("El nombre del conductor va primero.")
+}
+else if (hacker1 > hacker2) {
+    console.log("Yo, el navegador va primero definitivamente.")
+}
+else {
+    console.log("¿Qué? ¿Los dos tienen el mismo nombre?")
+}
